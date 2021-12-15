10a699a492a77e3b6d5dccb7f20223860bd29a63
naziiriah <naziistry7@gmail.com>
2021-12-15 08:49:40 +0100

    Update contactForm.js

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -10,8 +10,8 @@ const ContactForm = () => {
     return ( 
         <Box as="footer" width="100%" justifyContent="center" height="27rem" borderBottomColor="brown" borderBottomWidth="3px" borderBottomStyle="solid" borderTopColor="brown" borderTopWidth="3px" borderTopStyle="solid">
             <Text as="h1" textAlign="center" fontSize="39px"my="1rem" fontWeight="600" letterSpacing="1px">Interested in doing a project with me ?</Text>
-                <Box width="100%" display="flex" justifyContent="center"> <EmailDisplay/></Box>
                 
+                <EmailDisplay/>
             <Text as="h3" textAlign="center" my="2rem" fontWeight="600" fontSize="25px">Nazir, 2021.</Text>
         </Box>
      );
