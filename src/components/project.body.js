ba7445388c78a01476ed15b482d7589842ead1cc
naziiriah <naziistry7@gmail.com>
2021-12-17 22:44:50 +0100

    Update project.body.js

--- src/components/project.body.js
+++ src/components/project.body.js
@@ -1,10 +1,12 @@
 import { 
     Box,
-    Text
-    ,
+    Text,
+    Icon
  } from "@chakra-ui/react";
 import ContactForm from "./contactForm";
- 
+import { FaCartPlus, FaCode, FaEye, FaListUl, FaMapPin} from 'react-icons/fa'
+import { IoIosChatboxes } from "react-icons/io"
+import { RiMapPinFill } from "react-icons/ri"
 
 const Project = () => {
     return ( 
@@ -17,61 +19,78 @@ const Project = () => {
 
             <Box as="article" width={[ "90%", "80%","80%","60%"]} margin="auto" mt="10rem" height="120rem">
                 <Box as="section">
-                    <Box width="100%" height="15rem" bgColor="lightgrey"></Box>
-                    <Box width="100%" display="flex" flexDirection={["column", "column", "row", "row"]} justifyContent="space-between">
                     <Box  width="50%">
                     <Text as="h3" my=".4rem" fontSize="23px" width="100%">Audio Ecommerce Store</Text>
                     <Text as="h3" color="brown" fontStyle="italic" fontSize="20px" width="100%">Design / Frontend Development</Text>
                         </Box>
+                    <Box width="100%" height="15rem" bgColor="lightgrey">
+                         <Box width="10%" marginLeft="45%"  ><Icon as={ FaCartPlus } marginTop="5rem"  fontSize="53px" color="brown"/></Box>
+                    </Box>
+                    <Box width="100%" display="flex" flexDirection={["column", "column", "row", "row"]} justifyContent="space-between">
                     
-                    <Box  width="50%">
-                            <Text as="h3" my=".4rem" fontSize="23px" width="100%">Github: <Box as="a"></Box></Text>
-                            <Text as="h3" color="brown" fontStyle="italic" width="100%" fontSize="20px">Live site<Box as="a"></Box></Text>
+                    <Box width="100%" display="flex" flexDirection={["column", "column", "row", "row"]} justifyContent="space-between">
+                        <Box width="1%"> <Box > <Icon as={ FaCode } marginTop="1rem"  fontSize="25px" color="brown"/> </Box></Box>
+                        <Box width="1%" justifyContent="end">
+                            <Box><Icon as={ FaEye } marginTop="1rem"  fontSize="25px" color="brown"/></Box>
+                            
                         </Box>
+                    </Box>
                     
                     </Box>
                 </Box>
 
                 <Box as="section" mt="8rem" height="30rem">
-                    <Box width="100%" height="15rem" bgColor="lightgrey"></Box>
-                    <Box width="100%" display="flex" flexDirection={["column", "column", "row", "row"]} justifyContent="space-between">
-                        <Box width="50%">
+                    <Box width="50%">
                         <Text as="h3" my=".4rem" fontSize="23px" width="100%">Product Feedback App</Text>
                         <Text as="h3" color="brown" fontStyle="italic" width="100%" fontSize="20px">Frontend / Customer / Unit Testing</Text>
                         </Box>
-                        <Box width="50%">
-                            <Text as="h3" my=".4rem" fontSize="23px" width="100%">Github: <Box as="a"></Box></Text>
-                            <Text as="h3" color="brown" fontStyle="italic" width="100%" fontSize="20px">Live site<Box as="a"></Box></Text>
+                    <Box width="100%" height="15rem" bgColor="lightgrey">
+                    <Box width="10%" marginLeft="45%"  ><Icon as={ IoIosChatboxes } marginTop="5rem"  fontSize="53px" color="brown"/></Box>
+                    </Box>
+                    <Box width="100%" display="flex" flexDirection={["column", "column", "row", "row"]} justifyContent="space-between">
+                        <Box width="1%"> <Box > <Icon as={ FaCode } marginTop="1rem"  fontSize="25px" color="brown"/> </Box></Box>
+                        <Box width="1%" justifyContent="end">
+                            <Box><Icon as={ FaEye } marginTop="1rem"  fontSize="25px" color="brown"/></Box>
+                            
                         </Box>
 
                     </Box>
                 </Box>
 
                 <Box as="section" height="30rem" my="-rem">
-                    <Box width="100%" height="15rem" bgColor="lightgrey"></Box>
-                    <Box width="100%"  display="flex" flexDirection={["column", "column", "row", "row"]} justifyContent="space-between">
-                        <Box width="50%">
+                    <Box width="50%">
                         <Text as="h3" my=".4rem" fontSize="23px" width="100%">Ip Address Tracker</Text>
                         <Text as="h3" color="brown" fontStyle="italic"  width="100%" fontSize="20px">Frontend Development / API </Text>
                         </Box>
-                        <Box  width="50%">
-                            <Text as="h3" my=".4rem" fontSize="23px" width="100%">Github: <Box as="a"></Box></Text>
-                            <Text as="h3" color="brown" fontStyle="italic" width="100%" fontSize="20px">Live site<Box as="a"></Box></Text>
+                    <Box width="100%" height="15rem" bgColor="lightgrey">
+                    <Box width="10%" marginLeft="45%"  ><Icon as={ RiMapPinFill } marginTop="5rem"  fontSize="53px" color="brown"/></Box>
+                    </Box>
+                    <Box width="100%" display="flex" flexDirection={["column", "column", "row", "row"]} justifyContent="space-between">
+                        <Box width="1%"> <Box > <Icon as={ FaCode } marginTop="1rem"  fontSize="25px" color="brown"/> </Box></Box>
+                        <Box width="1%" justifyContent="end">
+                            <Box><Icon as={ FaEye } marginTop="1rem"  fontSize="25px" color="brown"/></Box>
+                            
                         </Box>
                     </Box>
                 </Box>
 
                 <Box as="section" my="0rem" height="30rem">
-                    <Box width="100%" height="15rem" bgColor="lightgrey"></Box>
-                    <Box width="100%" display="flex" flexDirection={["column", "column", "row", "row"]} justifyContent="space-between">
-                        <Box width="50%">
+                    <Box width="50%">
                             <Text as="h3" my=".4rem" fontSize="23px" width="100%">Static Job Listing</Text>
                             <Text as="h3" color="brown" fontStyle="italic" width="100%" fontSize="20px">Design </Text>
                         </Box >
-                        <Box  width="50%">
-                            <Text as="h3" my=".4rem" fontSize="23px" width="100%">Github: <Box as="a"></Box></Text>
-                            <Text as="h3" color="brown" fontStyle="italic" width="100%" fontSize="20px">Live site<Box as="a"></Box></Text>
+                    <Box width="100%" height="15rem" bgColor="lightgrey">
+                    <Box width="10%" marginLeft="45%"  ><Icon as={ FaListUl } marginTop="5rem"  fontSize="53px" color="brown"/></Box>
+                    </Box>
+                    <Box width="100%" display="flex" flexDirection={["column", "column", "row", "row"]} justifyContent="space-between">
+                        
+                    <Box width="100%" display="flex" flexDirection={["column", "column", "row", "row"]} justifyContent="space-between">
+                        <Box width="1%"> <Box > <Icon as={ FaCode } marginTop="1rem"  fontSize="25px" color="brown"/> </Box></Box>
+                        <Box width="1%" justifyContent="end">
+                            <Box><Icon as={ FaEye } marginTop="1rem"  fontSize="25px" color="brown"/></Box>
+                            
                         </Box>
+                    </Box>
                     </Box>
                     
                 </Box>
